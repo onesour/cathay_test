928544c3f6ff0fc3a27a792f83040e2b00d137b0
onesour <yiiihsuan.wu@gmail.com>
2024-04-30 16:59:17 +0800

    fix: Wait credit card element to prevent not loading completely.

--- test_cathay.py
+++ test_cathay.py
@@ -42,6 +42,8 @@ class TestCathay:
         credit_card_elem = self.driver.find_element(By.XPATH,
                                                     "/html/body/div[1]/header/div/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div/div[1]/div[1]/div")
         credit_card_elem.click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
+            (By.XPATH, "/html/body/div[1]/header/div/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div/div[1]/div[2]/a")))
         credit_card_sub_elems = self.driver.find_elements(By.XPATH,
                                                           "/html/body/div[1]/header/div/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div/div[1]/div[2]/a")
         logging.info(f"There are {len(credit_card_sub_elems)} items under credit card menu.")
